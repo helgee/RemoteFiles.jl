1e28f207bae6c7f9febac154c857f5735e4a5f80
Helge Eichhorn <git@helgeeichhorn.de>
2017-07-29 10:42:41 +0200

    Set-up doc structure

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter, RemoteFiles
+
+makedocs(
+    format = :html,
+    sitename = "RemoteFiles.jl",
+    authors = "Helge Eichhorn",
+    pages = [
+        "Home" => "index.md",
+        "API" => "api.md",
+    ],
+)
+
+deploydocs(
+    repo = "github.com/helgee/RemoteFiles.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+)
