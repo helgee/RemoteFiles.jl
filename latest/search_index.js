1531a7bf66eb787a3c4d4a80ad88679cb72b157b
autodocs <autodocs>
2018-07-13 07:49:36 +0000

    build based on b38c84c

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Installation",
     "category": "section",
-    "text": "The package can be installed through Julia's package manager:Pkg.add(\"RemoteFiles\")"
+    "text": "The package can be installed through Julia\'s package manager:Pkg.add(\"RemoteFiles\")"
 },
 
 {
@@ -49,115 +49,115 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "api.html#Base.Filesystem.isfile-Tuple{RemoteFiles.RemoteFileSet,Any}",
+    "location": "api.html#Base.Filesystem.isfile-Tuple{RemoteFileSet,Any}",
     "page": "API",
     "title": "Base.Filesystem.isfile",
-    "category": "Method",
-    "text": "isfile(rfs::RemoteFileSet, file)\n\nCheck whether a specific file contained in rfs has been downloaded.\n\n\n\n"
+    "category": "method",
+    "text": "isfile(rfs::RemoteFileSet, file)\n\nCheck whether a specific file contained in rfs has been downloaded.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#Base.Filesystem.isfile-Tuple{RemoteFiles.RemoteFileSet}",
+    "location": "api.html#Base.Filesystem.isfile-Tuple{RemoteFileSet}",
     "page": "API",
     "title": "Base.Filesystem.isfile",
-    "category": "Method",
-    "text": "isfile(rfs::RemoteFileSet, file)\n\nCheck whether all files contained in rfs have been downloaded.\n\n\n\n"
+    "category": "method",
+    "text": "isfile(rfs::RemoteFileSet, file)\n\nCheck whether all files contained in rfs have been downloaded.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#Base.Filesystem.isfile-Tuple{RemoteFiles.RemoteFile}",
+    "location": "api.html#Base.Filesystem.isfile-Tuple{RemoteFile}",
     "page": "API",
     "title": "Base.Filesystem.isfile",
-    "category": "Method",
-    "text": "isfile(rf::RemoteFile)\n\nCheck whether rf has been downloaded.\n\n\n\n"
+    "category": "method",
+    "text": "isfile(rf::RemoteFile)\n\nCheck whether rf has been downloaded.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#Base.Filesystem.rm-Tuple{RemoteFiles.RemoteFileSet,Any}",
+    "location": "api.html#Base.Filesystem.rm-Tuple{RemoteFileSet,Any}",
     "page": "API",
     "title": "Base.Filesystem.rm",
-    "category": "Method",
-    "text": "rm(rfs::RemoteFileSet, file; force=false)\n\nRemove a specific downloaded file contained in rfs.\n\n\n\n"
+    "category": "method",
+    "text": "rm(rfs::RemoteFileSet, file; force=false)\n\nRemove a specific downloaded file contained in rfs.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#Base.Filesystem.rm-Tuple{RemoteFiles.RemoteFileSet}",
+    "location": "api.html#Base.Filesystem.rm-Tuple{RemoteFileSet}",
     "page": "API",
     "title": "Base.Filesystem.rm",
-    "category": "Method",
-    "text": "rm(rfs::RemoteFileSet; force=false)\n\nRemove all downloaded files contained in rfs.\n\n\n\n"
+    "category": "method",
+    "text": "rm(rfs::RemoteFileSet; force=false)\n\nRemove all downloaded files contained in rfs.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#Base.Filesystem.rm-Tuple{RemoteFiles.RemoteFile}",
+    "location": "api.html#Base.Filesystem.rm-Tuple{RemoteFile}",
     "page": "API",
     "title": "Base.Filesystem.rm",
-    "category": "Method",
-    "text": "rm(rf::RemoteFile; force=false)\n\nRemove the downloaded file rf.\n\n\n\n"
+    "category": "method",
+    "text": "rm(rf::RemoteFile; force=false)\n\nRemove the downloaded file rf.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#Base.download-Tuple{RemoteFiles.RemoteFileSet}",
+    "location": "api.html#Base.download-Tuple{RemoteFileSet}",
     "page": "API",
     "title": "Base.download",
-    "category": "Method",
-    "text": "download(rfs::RemoteFileSet;\n    quiet::Bool=false, verbose::Bool=false, force::Bool=false)\n\nDownload all files contained in rfs.\n\nquiet: Do not print messages.\nverbose: Print all messages.\nforce: Force download and overwrite existing files.\n\n\n\n"
+    "category": "method",
+    "text": "download(rfs::RemoteFileSet;\n    quiet::Bool=false, verbose::Bool=false, force::Bool=false)\n\nDownload all files contained in rfs.\n\nquiet: Do not print messages.\nverbose: Print all messages.\nforce: Force download and overwrite existing files.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#Base.download-Tuple{RemoteFiles.RemoteFile}",
+    "location": "api.html#Base.download-Tuple{RemoteFile}",
     "page": "API",
     "title": "Base.download",
-    "category": "Method",
-    "text": "download(rf::RemoteFile; quiet::Bool=false, verbose::Bool=false,\n    force::Bool=false)\n\nDownload rf.\n\nquiet: Do not print messages.\nverbose: Print all messages.\nforce: Force download and overwrite existing files.\n\n\n\n"
+    "category": "method",
+    "text": "download(rf::RemoteFile; quiet::Bool=false, verbose::Bool=false,\n    force::Bool=false)\n\nDownload rf.\n\nquiet: Do not print messages.\nverbose: Print all messages.\nforce: Force download and overwrite existing files.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#RemoteFiles.files-Tuple{RemoteFiles.RemoteFileSet}",
+    "location": "api.html#RemoteFiles.files-Tuple{RemoteFileSet}",
     "page": "API",
     "title": "RemoteFiles.files",
-    "category": "Method",
-    "text": "files(rfs::RemoteFileSet)\n\nGet the (unsorted) list of file identifiers from a RemoteFileSet.\n\n\n\n"
+    "category": "method",
+    "text": "files(rfs::RemoteFileSet)\n\nGet the (unsorted) list of file identifiers from a RemoteFileSet.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#RemoteFiles.path-Tuple{RemoteFiles.RemoteFileSet,Any}",
+    "location": "api.html#RemoteFiles.path-Tuple{RemoteFileSet,Any}",
     "page": "API",
     "title": "RemoteFiles.path",
-    "category": "Method",
-    "text": "path(rfs::RemoteFileSet, file)\n\nGet the path to a specific downloaded file contained in rfs.\n\n\n\n"
+    "category": "method",
+    "text": "path(rfs::RemoteFileSet, file)\n\nGet the path to a specific downloaded file contained in rfs.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#RemoteFiles.path-Tuple{RemoteFiles.RemoteFile}",
+    "location": "api.html#RemoteFiles.path-Tuple{RemoteFile}",
     "page": "API",
     "title": "RemoteFiles.path",
-    "category": "Method",
-    "text": "path(rf::RemoteFile)\n\nGet the local path of rf.\n\n\n\n"
+    "category": "method",
+    "text": "path(rf::RemoteFile)\n\nGet the local path of rf.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api.html#RemoteFiles.paths-Tuple{RemoteFiles.RemoteFileSet,Vararg{Any,N} where N}",
+    "location": "api.html#RemoteFiles.paths-Tuple{RemoteFileSet,Vararg{Any,N} where N}",
     "page": "API",
     "title": "RemoteFiles.paths",
-    "category": "Method",
-    "text": "paths(rfs::RemoteFileSet, files...)\n\nGet the paths to specific downloaded files contained in rfs.\n\n\n\n"
+    "category": "method",
+    "text": "paths(rfs::RemoteFileSet, files...)\n\nGet the paths to specific downloaded files contained in rfs.\n\n\n\n\n\n"
 },
 
 {
     "location": "api.html#RemoteFiles.@RemoteFile-Tuple{Symbol,Any,Vararg{Any,N} where N}",
     "page": "API",
     "title": "RemoteFiles.@RemoteFile",
-    "category": "Macro",
-    "text": "@RemoteFile name url [key=value...]\n\nAssign the RemoteFile located at url to the variable name.\n\nThe following keyword arguments are available:\n\nfile: Set a different local file name.\ndir: The download directory. If dir is not set RemoteFiles will create   a new directory data under the root of the current package and save the   file there.\nupdates (default: :never): Indicates with which frequency the   remote file is updated. Possible values are:\n:never\n:daily\n:monthly\n:yearly\n:mondays/:weekly, :tuesdays, etc.\nretries (default: 3): How many retries should be attempted.\nwait (default: 5): How many seconds to wait between retries.\nfailed (default: :error): What to do if the download fails. Either throw   an exception (:error) or display a warning (:warn).\n\n\n\n"
+    "category": "macro",
+    "text": "@RemoteFile name url [key=value...]\n\nAssign the RemoteFile located at url to the variable name.\n\nThe following keyword arguments are available:\n\nfile: Set a different local file name.\ndir: The download directory. If dir is not set RemoteFiles will create   a new directory data under the root of the current package and save the   file there.\nupdates (default: :never): Indicates with which frequency the   remote file is updated. Possible values are:\n:never\n:daily\n:monthly\n:yearly\n:mondays/:weekly, :tuesdays, etc.\nretries (default: 3): How many retries should be attempted.\nwait (default: 5): How many seconds to wait between retries.\nfailed (default: :error): What to do if the download fails. Either throw   an exception (:error) or display a warning (:warn).\n\n\n\n\n\n"
 },
 
 {
     "location": "api.html#RemoteFiles.@RemoteFileSet-Tuple{Any,String,Any}",
     "page": "API",
     "title": "RemoteFiles.@RemoteFileSet",
-    "category": "Macro",
-    "text": "@RemoteFileSet name description begin\n    file1 = @RemoteFile ...\n    file2 = @RemoteFile ...\n    ...\nend\n\nCollect several RemoteFiles in the RemoteFileSet saved under name with a description.\n\n\n\n"
+    "category": "macro",
+    "text": "@RemoteFileSet name description begin\n    file1 = @RemoteFile ...\n    file2 = @RemoteFile ...\n    ...\nend\n\nCollect several RemoteFiles in the RemoteFileSet saved under name with a description.\n\n\n\n\n\n"
 },
 
 {
