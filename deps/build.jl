536a584b34598c829411823d27168de8f102f491
adam <aryczkowski@imgw.pl>
2018-08-19 16:05:19 +0300

    Fixes DevNull->devnull

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,3 @@
-if Sys.isunix() && !success(pipeline(`which curl`, DevNull))
+if Sys.isunix() && !success(pipeline(`which curl`, devnull))
     error("curl was not found. Please install curl through your system's package manager.")
 end
