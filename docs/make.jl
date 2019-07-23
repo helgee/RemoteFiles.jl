4614e07a591db8cd06856516819bdf924e209df8
Helge Eichhorn <git@helgeeichhorn.de>
2019-07-23 20:43:46 +0200

    Update CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,19 @@
 using Documenter, RemoteFiles
 
 makedocs(
-    format = :html,
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+    ),
     sitename = "RemoteFiles.jl",
     authors = "Helge Eichhorn",
     pages = [
         "Home" => "index.md",
         "API" => "api.md",
     ],
+    doctest = false,
 )
 
 deploydocs(
-    julia = "nightly",
     repo = "github.com/helgee/RemoteFiles.jl.git",
     target = "build",
-    deps = nothing,
-    make = nothing,
 )
