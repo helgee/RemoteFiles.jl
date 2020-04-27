28cb731ee757cec39cc57028db81d90772bd43dc
htyeim <htyeim@gmail.com>
2020-04-27 11:22:32 +0800

    add -R -z in curl

--- src/backends.jl
+++ src/backends.jl
@@ -6,14 +6,14 @@ abstract type AbstractBackend end
 struct CURL <: AbstractBackend end
 nameof(::CURL) = "cURL"
 
-function download(::CURL, url, filename; verbose::Bool=false)
+function download(::CURL, url, filename; verbose::Bool = false)
     curl = Sys.which("curl")
     curl === nothing && error("The `curl` executable was not found.")
     try
         if verbose
-            run(`$curl -o $filename -L $url`)
+            run(`$curl -R -z $filename -o $filename -L $url`)
         else
-            run(`$curl -s -o $filename -L $url`)
+            run(`$curl -s -R -z $filename -o $filename -L $url`)
         end
     catch err
         if (isdefined(Base, :ProcessFailedException) &&
@@ -28,7 +28,7 @@ end
 struct Wget <: AbstractBackend end
 nameof(::Wget) = "wget"
 
-function download(::Wget, url, filename; verbose::Bool=false)
+function download(::Wget, url, filename; verbose::Bool = false)
     wget = Sys.which("wget")
     wget === nothing && error("The `wget` executable was not found.")
     try
@@ -52,12 +52,12 @@ nameof(::Http) = "HTTP.jl"
 
 const HEADERS = ["User-Agent" => "RemoteFiles.jl/0.3 (+https://github.com/helgee/RemoteFiles.jl)", "Accept" => "*/*"]
 
-function download(::Http, url, filename; verbose::Bool=false)
+function download(::Http, url, filename; verbose::Bool = false)
     try
         if verbose
             HTTP.download(url, filename, HEADERS)
         else
-            HTTP.download(url, filename, HEADERS; update_period=typemax(Int))
+            HTTP.download(url, filename, HEADERS; update_period = typemax(Int))
         end
     catch err
         throw(DownloadError((sprint(showerror, err))))
